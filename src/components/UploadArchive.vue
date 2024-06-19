ef160bbd22debee3b70c5d4aa4efb4e09f29c14a
Mark Yarchak <markyarchak.work@gmail.com>
2024-06-19 15:26:54 +0300

    feat: add dark theme for archives dropzone

--- src/components/UploadArchive.vue
+++ src/components/UploadArchive.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { useDropZone } from '@vueuse/core';
+import { useApplicationTheme } from '@/composables/application-theme';
 
 const emit = defineEmits(['upload']);
 
@@ -9,10 +10,20 @@ const fileInputRef = ref<HTMLInputElement>();
 const inputUpdateKey = ref(0);
 const acceptDataTypes = ['application/zip', 'application/octet-stream', 'application/x-zip-compressed', 'multipart/x-zip'];
 
+const applicationTheme = useApplicationTheme();
 const { isOverDropZone } = useDropZone(dropZoneRef, {
   onDrop,
   dataTypes: acceptDataTypes,
 });
+
+const dropzoneClasses = computed(() => {
+  if (applicationTheme.isDark()) {
+    return isOverDropZone.value ? 'bg-blue-grey-lighten-1' : 'bg-grey-darken-1';
+  }
+  return isOverDropZone.value ? 'bg-blue-grey-lighten-4' : 'bg-grey-lighten-4';
+});
+const dropzoneTextClasses = computed(() => applicationTheme.isDark() ? 'text-grey-lighten-4' : 'text-grey-darken-4');
+const dropzoneIconColor = computed(() => applicationTheme.isDark() ? 'grey-lighten-3' : 'grey-darken-3');
 
 function onFileInput(event: Event) {
   if (event?.target instanceof HTMLInputElement) {
@@ -35,13 +46,13 @@ function processFiles(inputFiles: FileList | File[] | null) {
 
 <template>
   <div
-    :class="isOverDropZone ? 'bg-blue-grey-lighten-4' : 'bg-grey-lighten-4'"
+    :class="dropzoneClasses"
     ref="dropZoneRef"
     class="upload-dropzone"
     @click="fileInputRef?.click()"
   >
-    <v-icon size="60" color="grey-darken-3">mdi-package-up</v-icon>
-    <div class="text-grey-darken-4 mt-3">
+    <v-icon size="60" :color="dropzoneIconColor">mdi-package-up</v-icon>
+    <div :class="dropzoneTextClasses" class="mt-3">
       <span v-if="isOverDropZone">Put files here</span>
       <span v-else>Click to select files from your computer</span>
     </div>
