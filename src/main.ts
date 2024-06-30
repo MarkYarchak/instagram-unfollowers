260f3d7d1468dcdd5350cc7259b9edca03b19153
Mark Yarchak <markyarchak.work@gmail.com>
2024-06-30 23:45:00 +0300

    feat: add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import router from './router'
-import vuetify from '@/plugins/vuetify';
+import vuetify from '@/plugins/vuetify'
 
 import App from './App.vue'
 
+const pinia = createPinia()
 const app = createApp(App)
 
 app
+  .use(pinia)
   .use(router)
   .use(vuetify)
 
