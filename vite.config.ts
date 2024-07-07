5f3baf515a923328b503c60d0326738c5eb81f7e
Mark Yarchak <markyarchak.work@gmail.com>
2024-07-07 15:33:29 +0300

    feat: add more PWA options: name, description and theme_color

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,26 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  manifest: {
+    name: 'Instagram Unfollowers',
+    description: 'A safe and comfortable way to preview Instagram unfollowers',
+    theme_color: '#E8EAED',
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA(pwaOptions),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    },
+  },
 })
